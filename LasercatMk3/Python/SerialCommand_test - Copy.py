4537a2109dd3ac61512e68d489cb167b5de8475a
faridil <warpfactorsix@gmail.com>
2019-10-03 23:26:37 +0800

    lol

--- LasercatMk3/Python/SerialCommand_test - Copy.py
+++ LasercatMk3/Python/SerialCommand_test - Copy.py
@@ -9,39 +9,48 @@ import math
 #)
 
 #EndCom = "\xff\xff\xff"
-class ServoCommand:
-	StartX=0
-	StartY=0
-	EndX=0
-	EndY=0
-	Speed=0
-	
-	def __init__(self, dist): 
-		self.dist = dist
-	def servoMove(self,StartX,StartY,EndX,EndY):
-		DistX=math.sqrt(math.pow((EndX-StartX))+math.pow((EndY-StartY)))
-	def Distance(EndX,EndY):
-		self.Dist =math.sqrt(math.pow((EndX-0),2)+math.pow((EndY-0),2))
-		return math.sqrt(math.pow((EndX-0),2)+math.pow((EndY-0),2))
 
 
+x = random.randint(50,150)
+y = random.randint(100, 150)
+startX=90
+startY=90
+endX=random.randint(50,150)
+endY=random.randint(100, 150)
+speed = 1;
 
-ServoMove=ServoCommand
+distance = math.sqrt(math.pow(endX-startX,2)+math.pow(endY-startY,2));
+directionX = (endX-startX) / distance;
+directionY = (endY-startY) / distance;
+
+X = startX;
+Y = startY;
+
+while True:
+	moving = True
+	while moving:
+		X += directionX * speed
+		Y += directionY * speed
+		XY='X'+str(int(X))+':'+'Y'+str(int(Y))+'\n'
+		print (XY)
+		if(math.sqrt(math.pow(X-startX,2)+math.pow(Y-startY,2)) >= distance):
+			X = endX
+			Y = endY
+			startX=endX
+			startY=endY
+			XY='X'+str(X)+':'+'Y'+str(Y)+'\n'
+			print (XY)
+			moving = False
+		#time.sleep(0.100)
 
 
-while (True):
+#while (True):
 #	x = input("Enter X value: ")
 #	y = input("Enter Y value: ")
-	x = random.randint(50,150)
-	y = random.randint(100, 150)
-	ServoMove.StartX=x
-	ServoMove.Starty=y
-	ServoMove.Distance(x,y)
-	xy='X'+str(x)+':'+'Y'+str(y)+'\n'
-	print (ServoMove.Distance(x,y))
-	ServoMove.Dist=ServoMove.Distance(x,y)
-	print (ServoMove.Dist)
-	print (xy)
+#	x = random.randint(50,150)
+#	y = random.randint(100, 150)
+#	xy='X'+str(x)+':'+'Y'+str(y)+'\n'
+#	print (xy)
 
 #	ser.write(xy.encode())
-	time.sleep(0.600)
+	
